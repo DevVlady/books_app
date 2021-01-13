372cea1feb64145d8373cbb717d8aa83c0832014
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-13 10:09:38 -0600

    Updates

---
+++ precache-manifest.15354d6ba0197bf1548a982d29a4e155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10c5d3414d4a8da758b3b3ec1e8681e",
+    "url": "/books_app/index.html"
+  },
+  {
+    "revision": "0aa15da4be7afdede8fa",
+    "url": "/books_app/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "9a639d82447ade4efadb",
+    "url": "/books_app/static/js/2.c162a99d.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/books_app/static/js/2.c162a99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa15da4be7afdede8fa",
+    "url": "/books_app/static/js/main.04832385.chunk.js"
+  },
+  {
+    "revision": "6fa11cc1808cfae33a0d",
+    "url": "/books_app/static/js/runtime-main.9d0bc793.js"
+  }
+]);
